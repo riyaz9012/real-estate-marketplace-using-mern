3e4e84239a54ba2e6deaf14cf3d6f85f0cee74c2
mohd <mohdriyaz901251@gmail.com>
2024-08-03 09:22:00 -0400

    first commit

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+export default function Signup() {
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'></input>
+      </form>
+    </div>
+      )
+}
